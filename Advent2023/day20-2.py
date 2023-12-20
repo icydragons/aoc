d2ba4151461265d2e731c80ce6b7bf534dbe6a0b
Robin Schriebman <icydragons@gmail.com>
2023-12-20 15:15:06 -0800

    Add graph vis link

--- Advent2023/day20-2.py
+++ Advent2023/day20-2.py
@@ -145,7 +145,8 @@ def run(filename):
       lcm = lcm*i//math.gcd(lcm, i)
   print(lcm)
   
-
+# Graph viz link
+# https://dreampuf.github.io/GraphvizOnline/#digraph%20G%20%7B%0A%20%20vh%20-%3E%20qc%2C%20rr%0Apb%20-%3E%20gf%2C%20gv%2C%20vp%2C%20qb%2C%20vr%2C%20hq%2C%20zj%0Azj%20-%3E%20kn%2C%20pb%0Amm%20-%3E%20dj%0Agp%20-%3E%20cp%0Adc%20-%3E%20ns%0Aqc%20-%3E%20gp%0Adx%20-%3E%20fq%2C%20dj%0Atg%20-%3E%20nl%2C%20ks%0Apr%20-%3E%20nl%0Agx%20-%3E%20xf%0Ahd%20-%3E%20lt%2C%20nl%0Adq%20-%3E%20dj%2C%20jc%0Aht%20-%3E%20jv%0Abs%20-%3E%20pb%2C%20rd%0Anl%20-%3E%20ks%2C%20cq%2C%20tc%2C%20xf%2C%20gx%2C%20hd%2C%20lt%0Adj%20-%3E%20dc%2C%20fq%2C%20jz%2C%20ht%2C%20zs%2C%20jc%0Arr%20-%3E%20gp%2C%20rv%2C%20jt%2C%20qc%2C%20sq%0Avr%20-%3E%20qb%0Ajz%20-%3E%20dj%2C%20ht%0Ahq%20-%3E%20nx%0Acf%20-%3E%20jg%2C%20rr%0Ahj%20-%3E%20cf%2C%20rr%0Amt%20-%3E%20rr%0Asq%20-%3E%20rr%2C%20vh%0Ajg%20-%3E%20rr%2C%20pd%0Agf%20-%3E%20gv%0Axv%20-%3E%20dj%2C%20dx%0Arh%20-%3E%20nl%2C%20gx%0Abroadcaster%20-%3E%20hd%2C%20zj%2C%20sq%2C%20jz%0Ajv%20-%3E%20dj%2C%20zs%0Ard%20-%3E%20vs%2C%20pb%0Apd%20-%3E%20rr%2C%20mt%0Arv%20-%3E%20ns%0Avp%20-%3E%20ns%0Avs%20-%3E%20pb%0Anx%20-%3E%20pb%2C%20bs%0Azp%20-%3E%20mm%2C%20dj%0Ans%20-%3E%20rx%0Alt%20-%3E%20rh%0Apf%20-%3E%20pr%2C%20nl%0Atc%20-%3E%20qz%0Axz%20-%3E%20dj%2C%20zp%0Aqb%20-%3E%20hq%0Arl%20-%3E%20pf%2C%20nl%0Afq%20-%3E%20xz%0Akn%20-%3E%20pb%2C%20xn%0Axf%20-%3E%20tg%0Aqz%20-%3E%20nl%2C%20rl%0Aks%20-%3E%20tc%0Ajt%20-%3E%20kb%0Ajc%20-%3E%20xv%0Akb%20-%3E%20hj%2C%20rr%0Azs%20-%3E%20dq%0Agv%20-%3E%20vr%0Acq%20-%3E%20ns%0Acp%20-%3E%20rr%2C%20jt%0Axn%20-%3E%20pb%2C%20gf%0A%7D
 
 def main():
   run("day20.txt")
