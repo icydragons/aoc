d241d9b5f6de1ab0c6d70b37605a67401425f6ca
Robin Schriebman <icydragons@gmail.com>
2025-12-01 23:17:06 -0800

    more cleaning

--- Advent2025/2.py
+++ Advent2025/2.py
@@ -9,32 +9,24 @@ def main():
   with open("Advent2025/2.txt") as f:
     for line in f:
       ranges = line.split(',')
-  
-    print(ranges)
 
     s = 0
     for i in range(len(ranges)):
       start,end = [int(x) for x in ranges[i].split('-')]
-      sols = []
       while start <= end:
-        sols.extend(int(x+x) for x in reg.findall(str(start)))
+        if reg.match(str(start)):
+          s+=start
         start+=1
-      print(sols)
-      s += sum(sols)
 
     print(f'Part 1: {s}')
 
     s = 0
     for i in range(len(ranges)):
       start,end = [int(x) for x in ranges[i].split('-')]
-      print(start, end)
-      sols = []
       while start <= end:
         if reg2.match(str(start)):
-          sols.append(start)
+          s+=start
         start+=1
-      print(sols)
-      s += sum(sols)
 
     print(f'Part 2: {s}')
 
