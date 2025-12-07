6b75fdd0c61c240b88748c279dbfba3f71280d36
Robin Schriebman <icydragons@gmail.com>
2025-12-07 13:59:26 -0800

    Part 7

---
+++ Advent2025/7.py
@@ -0,0 +1,44 @@
+import math
+import re
+
+def prop(s, sOld, val, i):
+   old = sOld[i]
+   if val == '^' and old > 0:
+      s[i-1], s[i], s[i+1] = old + s[i-1], 0, old + s[i+1]
+      return 1
+   return 0
+   
+
+def main():
+  grid = []
+  with open("Advent2025/7.txt") as f:
+    for line in f:
+       grid.append([x for x in line.strip()])
+    
+    s = [0] * len(grid[0])
+    s[grid[0].index('S')] = 1
+    t = 0
+    for row in grid[1:]:
+       print(s)
+       sOld = s
+       hits = list(map(lambda v,i: prop(s, sOld, v, i), row, range(len(row))))
+       t += sum(hits)
+
+    print(f'solution to 1: {t}')
+    
+    s = [0] * len(grid[0])
+    s[grid[0].index('S')] = 1
+    for row in grid[1:]:
+       print(s)
+       sOld = s.copy()
+       hits = list(map(lambda v,i: prop(s, sOld, v, i), row, range(len(row))))
+
+    print(f'solution to 2: {sum(s)}')
+
+
+    return 0
+                
+
+
+if __name__ == "__main__":
+    main()
