ae6656543ac4936a18052169d85ef5b83383a41e
Robin Schriebman <icydragons@gmail.com>
2025-12-08 11:26:56 -0800

    Updated to heap and removed some of the classes

--- Advent2025/8.py
+++ Advent2025/8.py
@@ -1,5 +1,7 @@
+import heapq
 import math
 import re
+import time
 
 import sortedcontainers
 
@@ -16,25 +18,9 @@ class Junction():
    def distance(self, o):
       return (self.x-o.x)**2 + (self.y - o.y)**2 + (self.z - o.z)**2
    
-class Distance():
-   def __init__(self, d, a, b):
-      self.d = d
-      self.a = a
-      self.b = b
-
-   def __lt__(self, other):
-        return self.d < other.d 
-   
-   def __repr__(self):
-      return f'{self.d} -> {self.a}<->{self.b}'
-   
-
-def addDistance(distances, a, b):
-   distance = Distance(a.distance(b), a, b)
-   distances.add(distance)
 
 def addCircuit(circuits, distance):
-   a,b = distance.a,distance.b
+   a,b = distance[0],distance[1]
    aCircuit = next((x for x in circuits if a.id in x), None)
    bCircuit = next((x for x in circuits if b.id in x), None)
    if not aCircuit and not bCircuit:
@@ -42,14 +28,15 @@ def addCircuit(circuits, distance):
    elif aCircuit and bCircuit:
       if aCircuit != bCircuit:
          circuits.remove(aCircuit)
-         circuits.remove(bCircuit)
-         circuits.append(aCircuit + bCircuit) 
+         bCircuit.extend(aCircuit) 
    elif not aCircuit:
       bCircuit.append(a.id) 
    elif not bCircuit:
       aCircuit.append(b.id)
    
 def run(file, times): 
+  
+  before = time.perf_counter_ns()
   boxes = []
   with open(f"Advent2025/{file}") as f:
     i = 0
@@ -57,36 +44,41 @@ def run(file, times):
        x,y,z = line.strip().split(',')
        boxes.append(Junction(i, x, y, z))
        i+=1
+    elapsed = time.perf_counter_ns() - before
+    print(f'Parsing done in ({elapsed//1_000_000} ms)')
 
-    distances = sortedcontainers.SortedList()
+    distances = []
     for i1 in range(len(boxes)):
        for i2 in range(i1+1, len(boxes)):
-          addDistance(distances, boxes[i1], boxes[i2])
+         d = boxes[i1].distance(boxes[i2])   
+         heapq.heappush(distances, (d, [boxes[i1], boxes[i2]]))
+    
+    
+    elapsed = time.perf_counter_ns() - before
+    print(f'Distances done in ({elapsed//1_000_000} ms)')
 
     # Part 1
     circuits = []
     if times:
       for i in range(times):
-        addCircuit(circuits, distances[i])
-
+        addCircuit(circuits, heapq.heappop(distances)[1])
 
       circuits.sort(key=lambda r: -len(r))
       s = 1
       for i in range(3):
-        print(circuits[i])
         s *= len(circuits[i])
-
-      print(f'Solution to 1: {s}')
+        
+      elapsed = time.perf_counter_ns() - before
+      print(f'Solution to 1: {s} in ({elapsed//1_000_000} ms)')
+      
 
       # Part 2 - keep going
-      i = times
       while len(circuits[0]) < len(boxes):
-        addCircuit(circuits, distances[i])
-        i+=1
+        d = heapq.heappop(distances)
+        addCircuit(circuits, d[1])
       
-      lastD = distances[i - 1]
-      print(lastD)
-      print(f'Solution to 2: {lastD.a.x * lastD.b.x}')
+      elapsed = time.perf_counter_ns() - before
+      print(f'Solution to 2: {d[1][0].x * d[1][1].x} in ({elapsed//1_000_000} ms)')
      
 
 
